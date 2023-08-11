0faeed413197c1f78492051af49168c2d987b32f
BeefCutlet <whysabi492@naver.com>
2023-08-11 12:11:08 +0900

    Initial commit

---
+++ src/test/java/shop/goodspia/goods/GoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.goodspia.goods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
