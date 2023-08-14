df46240bb40a25a766a1b26bdfa895ddb7de7451
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:15:18 +0900

    [DOCS] ArtistDto 추가

---
+++ src/main/java/shop/goodspia/goods/dto/ArtistDto.java
@@ -0,0 +1,14 @@
+package shop.goodspia.goods.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class ArtistDto {
+    private Long id;
+    private String nickname;
+    private String profileImage;
+    private String accountBank;
+    private String accountNumber;
+    private String phoneNumber;
+}
