27bde3d297c7113f92f5742a9359009f69e8a099
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:24:48 +0900

    [DOCS] MemberDto 추가

---
+++ src/main/java/shop/goodspia/goods/dto/MemberDto.java
@@ -0,0 +1,11 @@
+package shop.goodspia.goods.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberDto {
+
+    private String email;
+    private String nickname;
+}
