7c29c7ad82468ef8bedfc674cecc7a81c3eca4ac
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:15:28 +0900

    [DOCS] GoodsDto 추가

---
+++ src/main/java/shop/goodspia/goods/dto/GoodsDto.java
@@ -0,0 +1,13 @@
+package shop.goodspia.goods.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class GoodsDto {
+
+    private String name;
+    private String summary;
+    private String content;
+    private String category;
+}
