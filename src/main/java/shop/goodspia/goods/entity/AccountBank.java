2ae86fa69c6b2eaea02ca18ebdd74f5ff354a0f2
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:24:31 +0900

    [FEAT] 문자열을 AccountBank 변환 가능한지 검증하고 변환하는 메서드 추가

--- src/main/java/shop/goodspia/goods/entity/AccountBank.java
+++ src/main/java/shop/goodspia/goods/entity/AccountBank.java
@@ -1,5 +1,27 @@
 package shop.goodspia.goods.entity;
 
 public enum AccountBank {
-    KB, SHINHAN, NH
+    KB("KB"),
+    SHINHAN("SHINHAN"),
+    NH("NH");
+
+    private final String bankName;
+
+    AccountBank(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public static AccountBank covertToBank(String bank) {
+        if (bank == null) {
+            throw new IllegalArgumentException("은행 이름이 입력되지 않았습니다.");
+        }
+
+        for (AccountBank accountBank : AccountBank.values()) {
+            if (accountBank.bankName.equals(bank)) {
+                return accountBank;
+            }
+        }
+
+        throw new IllegalArgumentException("일치하는 은행이 없습니다.");
+    }
 }
