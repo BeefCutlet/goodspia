cce0fe144e862cecc62a2288140d085b234ce256
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:14:19 +0900

    [FEAT] Artist 엔티티 생성 메서드 추가

--- src/main/java/shop/goodspia/goods/entity/Artist.java
+++ src/main/java/shop/goodspia/goods/entity/Artist.java
@@ -1,21 +1,47 @@
 package shop.goodspia.goods.entity;
 
-import lombok.Getter;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import shop.goodspia.goods.dto.ArtistDto;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Slf4j
 public class Artist extends BaseTimeEntity {
 
     @Id @GeneratedValue
     @Column(name = "artist_id")
     private Long id;
     private String nickname;
+    private String profileImage;
     @Enumerated(EnumType.STRING)
     private AccountBank accountBank;
     private String accountNumber;
     private String phoneNumber;
     private LocalDateTime createdTime;
+
+    public static Artist createArtist(ArtistDto artistDto) {
+        return Artist.builder()
+                .nickname(artistDto.getNickname())
+                .profileImage(artistDto.getProfileImage())
+                .accountBank(AccountBank.covertToBank(artistDto.getAccountBank()))
+                .accountNumber(artistDto.getAccountNumber())
+                .phoneNumber(artistDto.getPhoneNumber())
+                .build();
+    }
+
+    public void updateArtist(ArtistDto artistDto) {
+        this.nickname = artistDto.getNickname();
+        this.profileImage = artistDto.getProfileImage();
+        this.accountBank = AccountBank.covertToBank(artistDto.getAccountBank());
+        this.accountNumber = artistDto.getAccountNumber();
+        this.phoneNumber = artistDto.getPhoneNumber();
+    }
+
 }
