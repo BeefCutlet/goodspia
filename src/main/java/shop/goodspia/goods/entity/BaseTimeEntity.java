e8a8380e5d26418a72e77951aeb48f738fa91471
BeefCutlet <whysabi492@naver.com>
2023-08-15 01:07:38 +0900

    [FEAT] Auditing 기능 추가 (생성시각, 업데이트 시각)

--- src/main/java/shop/goodspia/goods/entity/BaseTimeEntity.java
+++ src/main/java/shop/goodspia/goods/entity/BaseTimeEntity.java
@@ -1,14 +1,24 @@
 package shop.goodspia.goods.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 public abstract class BaseTimeEntity {
 
+    @CreatedDate
+    @Column(updatable = false)
     private LocalDateTime createdTime;
+
+    @LastModifiedDate
     private LocalDateTime lastUpdatedTime;
 }
