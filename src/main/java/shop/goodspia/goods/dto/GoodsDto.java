4b59eba0153814a44a355c3051e59a1102c634ae
BeefCutlet <whysabi492@naver.com>
2023-08-15 23:41:22 +0900

    [FEAT] GoodsDto 저장 필드 추가

--- src/main/java/shop/goodspia/goods/dto/GoodsDto.java
+++ src/main/java/shop/goodspia/goods/dto/GoodsDto.java
@@ -6,8 +6,10 @@ import lombok.Setter;
 @Getter @Setter
 public class GoodsDto {
 
+    private Long id;
     private String name;
     private String summary;
     private String content;
     private String category;
+    private String image;
 }
