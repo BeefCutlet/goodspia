a5c516c7c7c4d1cc53fd35db0edaa07d13d30bac
BeefCutlet <whysabi492@naver.com>
2023-08-15 22:25:24 +0900

    [FEAT] 굿즈 엔티티 수정, 삭제 메서드 추가 / 굿즈 메인이미지 필드 및 삭제 여부 체크 + 삭제 시각 필드 추가

--- src/main/java/shop/goodspia/goods/entity/Goods.java
+++ src/main/java/shop/goodspia/goods/entity/Goods.java
@@ -4,6 +4,7 @@ import lombok.*;
 import shop.goodspia.goods.dto.GoodsDto;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +24,9 @@ public class Goods extends BaseTimeEntity {
     private String summary;
     private String content;
     private String category;
+    private String image;
+    private int isDeleted;
+    private LocalDateTime deletedTime;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "artist_id")
@@ -37,7 +41,24 @@ public class Goods extends BaseTimeEntity {
                 .summary(goodsDto.getSummary())
                 .content(goodsDto.getContent())
                 .category(goodsDto.getCategory())
+                .image(goodsDto.getImage())
+                .isDeleted(0)
                 .artist(artist)
                 .build();
+    }
+
+    //굿즈 정보 갱신용 메서드
+    public void updateGoods(GoodsDto goodsDto) {
+        this.name = goodsDto.getName();
+        this.summary = goodsDto.getSummary();
+        this.content = goodsDto.getContent();
+        this.category = goodsDto.getCategory();
+        this.image = goodsDto.getImage();
+    }
+
+    //굿즈 삭제용 메서드 - 삭제 여부 상태 변경
+    public void delete() {
+        this.isDeleted = 0;
+        this.deletedTime = LocalDateTime.now();
     }
 }
