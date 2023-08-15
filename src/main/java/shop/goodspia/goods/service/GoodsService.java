7a9c6f3f853e45a84a7f268713d55e63d8929007
BeefCutlet <whysabi492@naver.com>
2023-08-15 23:40:24 +0900

    [FEAT] 굿즈 정보 수정 메서드, 삭제 메서드 추가

--- src/main/java/shop/goodspia/goods/service/GoodsService.java
+++ src/main/java/shop/goodspia/goods/service/GoodsService.java
@@ -6,6 +6,7 @@ import org.springframework.transaction.annotation.Transactional;
 import shop.goodspia.goods.dto.GoodsDto;
 import shop.goodspia.goods.entity.Goods;
 import shop.goodspia.goods.entity.Member;
+import shop.goodspia.goods.exception.GoodsNotFoundException;
 import shop.goodspia.goods.exception.MemberNotFoundException;
 import shop.goodspia.goods.repository.GoodsRepository;
 import shop.goodspia.goods.repository.MemberRepository;
@@ -20,15 +21,43 @@ public class GoodsService {
     private final MemberRepository memberRepository;
     private final GoodsRepository goodsRepository;
 
-    public Long addGoods(GoodsDto goodsDto, String email) {
+    /**
+     * 굿즈 등록용 메서드
+     * @param artistId
+     * @param goodsDto
+     * @return
+     */
+    public Long addGoods(long artistId, GoodsDto goodsDto) {
         //회원 조회 - 아티스트 등록 여부 파악용
-        Optional<Member> findMember = memberRepository.findByEmail(email);
+        Optional<Member> findMember = memberRepository.findById(artistId);
         return findMember.map(
                 member -> {
                     Goods goods = Goods.createGoods(goodsDto, member.getArtist());
-                    Goods savedGoods = goodsRepository.save(goods);
-                    return savedGoods.getId();
+                    return goodsRepository.save(goods).getId();
                 }
         ).orElseThrow(() -> new MemberNotFoundException("회원이 존재하지 않습니다."));
+    }
+
+    /**
+     * 굿즈 정보 수정용 메서드
+     * @param goodsDto
+     */
+    public void modifyGoods(GoodsDto goodsDto) {
+        Optional.ofNullable(goodsRepository.findById(goodsDto.getId()))
+                .ifPresentOrElse(
+                        goods -> goods.get().updateGoods(goodsDto),
+                        () -> new GoodsNotFoundException("굿즈 정보가 없습니다.")
+                );
+    }
+
+    /**
+     * 굿즈 삭제 메서드 - 상태 변경
+     */
+    public void delete(long goodsId) {
+        Optional.ofNullable(goodsRepository.findById(goodsId))
+                .ifPresentOrElse(
+                        goods -> goods.get().delete(),
+                        () -> new GoodsNotFoundException("굿즈 정보가 없습니다.")
+                );
     }
 }
