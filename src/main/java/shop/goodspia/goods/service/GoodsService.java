4d358a1189c2c9ab3a0e53cbd80f287936d204ec
BeefCutlet <whysabi492@naver.com>
2023-08-16 21:51:08 +0900

    [FEAT] 굿즈 정보 저장 시에 디자인 옵션 저장하도록 수정

--- src/main/java/shop/goodspia/goods/service/GoodsService.java
+++ src/main/java/shop/goodspia/goods/service/GoodsService.java
@@ -4,10 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.goodspia.goods.dto.GoodsDto;
+import shop.goodspia.goods.entity.Design;
 import shop.goodspia.goods.entity.Goods;
 import shop.goodspia.goods.entity.Member;
+import shop.goodspia.goods.exception.ArtistNotFoundException;
 import shop.goodspia.goods.exception.GoodsNotFoundException;
 import shop.goodspia.goods.exception.MemberNotFoundException;
+import shop.goodspia.goods.repository.ArtistRepository;
+import shop.goodspia.goods.repository.DesignRepository;
 import shop.goodspia.goods.repository.GoodsRepository;
 import shop.goodspia.goods.repository.MemberRepository;
 
@@ -20,6 +24,8 @@ public class GoodsService {
 
     private final MemberRepository memberRepository;
     private final GoodsRepository goodsRepository;
+    private final DesignRepository designRepository;
+    private final ArtistRepository artistRepository;
 
     /**
      * 굿즈 등록용 메서드
@@ -29,13 +35,17 @@ public class GoodsService {
      */
     public Long addGoods(long artistId, GoodsDto goodsDto) {
         //회원 조회 - 아티스트 등록 여부 파악용
-        Optional<Member> findMember = memberRepository.findById(artistId);
-        return findMember.map(
-                member -> {
-                    Goods goods = Goods.createGoods(goodsDto, member.getArtist());
+        return artistRepository.findById(artistId).map(
+                artist -> {
+                    //굿즈 엔티티 생성
+                    Goods goods = Goods.createGoods(goodsDto, artist);
+                    //작성한 디자인 옵션의 엔티티 생성 후 DB에 저장
+                    for (String design : goodsDto.getDesigns()) {
+                        designRepository.save(Design.createDesign(design, goods));
+                    }
                     return goodsRepository.save(goods).getId();
                 }
-        ).orElseThrow(() -> new MemberNotFoundException("회원이 존재하지 않습니다."));
+        ).orElseThrow(() -> new ArtistNotFoundException("아티스트 정보가 존재하지 않습니다."));
     }
 
     /**
