29997fdf8f0fd72edd7883798842299ea4072715
BeefCutlet <whysabi492@naver.com>
2023-08-17 22:30:55 +0900

    [DOCS] 아티스트 리스트 조회용 메서드 추가

--- src/main/java/shop/goodspia/goods/service/ArtistService.java
+++ src/main/java/shop/goodspia/goods/service/ArtistService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import shop.goodspia.goods.dto.ArtistDto;
+import shop.goodspia.goods.dto.ArtistResponseDto;
 import shop.goodspia.goods.entity.Artist;
 import shop.goodspia.goods.entity.Member;
 import shop.goodspia.goods.exception.ArtistNotFoundException;
@@ -24,12 +25,30 @@ public class ArtistService {
     private final MemberRepository memberRepository;
 
     /**
+     * 아티스트 정보 조회 메서드
+     * @param artistId
+     * @return
+     */
+    public ArtistResponseDto getArtistInfo(long artistId) {
+        Artist artist = artistRepository.findById(artistId)
+                .orElseThrow(() -> new ArtistNotFoundException("Artist Data Not Found"));
+
+        return ArtistResponseDto.builder()
+                .nickname(artist.getNickname())
+                .profileImage(artist.getProfileImage())
+                .accountBank(artist.getAccountBank().toString())
+                .accountNumber(artist.getAccountNumber())
+                .phoneNumber(artist.getPhoneNumber())
+                .build();
+    }
+
+    /**
      * 아티스트 등록 메서드
      */
     public Long registerArtist(Long memberId, ArtistDto artistDto) {
         //회원 정보에 아티스트 번호 저장
         Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> new MemberNotFoundException("회원정보가 존재하지 않습니다."));
+                .orElseThrow(() -> new MemberNotFoundException("Member Data Not Found"));
 
         //아티스트 등록
         Artist savedArtist = artistRepository.save(Artist.createArtist(artistDto));
@@ -44,7 +63,7 @@ public class ArtistService {
      */
     public void modifyArtist(ArtistDto artistDto) {
         Artist artist = artistRepository.findById(artistDto.getId())
-                .orElseThrow(() -> new ArtistNotFoundException("아티스트로 등록되지 않았습니다."));
+                .orElseThrow(() -> new ArtistNotFoundException("Artist Data Not Found"));
         artist.updateArtist(artistDto);
     }
 }
