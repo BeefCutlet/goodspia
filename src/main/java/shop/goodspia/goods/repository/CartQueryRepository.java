6cbc7411b830033212a38891b7b4633b7d5cc856
BeefCutlet <whysabi492@naver.com>
2023-08-19 00:16:17 +0900

    [DEBUG] EntityManager 의존성 주입 에러 수정

--- src/main/java/shop/goodspia/goods/repository/CartQueryRepository.java
+++ src/main/java/shop/goodspia/goods/repository/CartQueryRepository.java
@@ -2,20 +2,24 @@ package shop.goodspia.goods.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import shop.goodspia.goods.dto.CartResponseDto;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 import static shop.goodspia.goods.entity.QCart.cart;
+import static shop.goodspia.goods.entity.QDesign.design;
 import static shop.goodspia.goods.entity.QGoods.goods;
 
 @Repository
-@RequiredArgsConstructor
 public class CartQueryRepository {
 
     private final JPAQueryFactory queryFactory;
+
+    public CartQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
 
     public List<CartResponseDto> findCartList(long memberId) {
         return queryFactory
@@ -23,12 +27,12 @@ public class CartQueryRepository {
                         cart.quantity.as("quantity"),
                         cart.goods.name.as("goodsName"),
                         cart.goods.price.as("price"),
-                        cart.goods.image.as("image"),
-                        cart.design.design.as("design")
+                        cart.goods.image.as("mainImage"),
+                        cart.design.designName.as("designName")
                 ))
                 .from(cart)
                 .join(cart.goods, goods)
-                .join(cart.goods.designs)
+                .join(cart.design, design)
                 .where(cart.member.id.eq(memberId))
                 .fetch();
 
