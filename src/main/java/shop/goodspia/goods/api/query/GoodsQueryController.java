66dcc92396fd599be65c213a717be170db8a8692
BeefCutlet <whysabi492@naver.com>
2023-08-20 22:50:23 +0900

    [FEAT] ArtistId로 굿즈 리스트 조회 API 추가

--- src/main/java/shop/goodspia/goods/api/query/GoodsQueryController.java
+++ src/main/java/shop/goodspia/goods/api/query/GoodsQueryController.java
@@ -1,6 +1,7 @@
 package shop.goodspia.goods.api.query;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +13,7 @@ import shop.goodspia.goods.dto.GoodsDetailResponseDto;
 import shop.goodspia.goods.dto.GoodsResponseDto;
 import shop.goodspia.goods.service.GoodsService;
 
-import java.util.List;
-
+@Slf4j
 @RestController
 @RequestMapping("/goods")
 @RequiredArgsConstructor
@@ -23,13 +23,22 @@ public class GoodsQueryController {
 
     @GetMapping("/list")
     public ResponseEntity<Page<GoodsResponseDto>> getGoodsList(Pageable pageable) {
+        log.info("GoodsQueryController.getGoodsList Start, pageSize={}, pageNum={}", pageable.getPageSize(), pageable.getPageNumber());
         Page<GoodsResponseDto> goodsList = goodsService.getGoodsList(pageable);
         return ResponseEntity.ok(goodsList);
     }
 
-    @GetMapping("/goods/{goodsId}")
+    @GetMapping("/detail/{goodsId}")
     public ResponseEntity<GoodsDetailResponseDto> getOneGoods(@PathVariable long goodsId) {
+        log.info("GoodsQueryController.getOneGoods Start, goodsId={}", goodsId);
         GoodsDetailResponseDto goods = goodsService.getGoods(goodsId);
         return ResponseEntity.ok(goods);
+    }
+
+    @GetMapping("/goods-list/{artistId}")
+    public ResponseEntity<Page<GoodsResponseDto>> getArtistGoodsList(@PathVariable long artistId, Pageable pageable) {
+        log.info("GoodsQueryController.getArtistGoodsList Start, artistId={}", artistId);
+        Page<GoodsResponseDto> artistGoodsList = goodsService.getArtistGoodsList(pageable, artistId);
+        return ResponseEntity.ok(artistGoodsList);
     }
 }
