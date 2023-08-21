774d3befaf912a7772dab7c2434b1fd60ca8b28a
BeefCutlet <whysabi492@naver.com>
2023-08-21 23:32:54 +0900

    [FEAT] OpenFeign 사용을 위한 어노테이션 추가

--- src/main/java/shop/goodspia/goods/GoodsApplication.java
+++ src/main/java/shop/goodspia/goods/GoodsApplication.java
@@ -2,9 +2,11 @@ package shop.goodspia.goods;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients("shop.goodspia.goods")
 @SpringBootApplication
 public class GoodsApplication {
 
