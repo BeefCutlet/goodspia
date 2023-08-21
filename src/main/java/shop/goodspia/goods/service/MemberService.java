d12da73cc230f72149af27c4ad08599edce2ea02
BeefCutlet <whysabi492@naver.com>
2023-08-21 23:32:29 +0900

    [FEAT] 회원 정보 저장, 수정 메서드 추가

--- src/main/java/shop/goodspia/goods/service/MemberService.java
+++ src/main/java/shop/goodspia/goods/service/MemberService.java
@@ -3,6 +3,7 @@ package shop.goodspia.goods.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import shop.goodspia.goods.dto.MemberDto;
 import shop.goodspia.goods.entity.Member;
 import shop.goodspia.goods.exception.MemberNotFoundException;
 import shop.goodspia.goods.repository.MemberRepository;
@@ -15,6 +16,26 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     /**
+     * 회원 정보 저장
+     * @param memberDto
+     * @return
+     */
+    public Long saveMember(MemberDto memberDto) {
+        Member member = Member.createMember(memberDto);
+        return memberRepository.save(member).getId();
+    }
+
+    /**
+     * 회원 정보 수정
+     * @param memberDto
+     */
+    public void modifyMemberInfo(MemberDto memberDto) {
+        Member member = memberRepository.findById(memberDto.getId())
+                .orElseThrow(() -> new MemberNotFoundException("Member Data Not Found"));
+        member.updateMember(memberDto);
+    }
+
+    /**
      * 단일 회원 정보 조회용 메서드
      * @param memberId
      * @return
@@ -23,5 +44,4 @@ public class MemberService {
         return memberRepository.findById(memberId)
                 .orElseThrow(() -> new MemberNotFoundException("Member Data Not Found"));
     }
-
 }
