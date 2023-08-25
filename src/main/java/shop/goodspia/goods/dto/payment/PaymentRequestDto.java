3861ab0a1ec92c2a09869c4abe337db008d0575d
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:14:30 +0900

    결제 API 요청용 DTO 전달 데이터 변경

--- src/main/java/shop/goodspia/goods/dto/payment/PaymentRequestDto.java
+++ src/main/java/shop/goodspia/goods/dto/payment/PaymentRequestDto.java
@@ -1,20 +1,22 @@
 package shop.goodspia.goods.dto.payment;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
-import shop.goodspia.goods.entity.Goods;
-import shop.goodspia.goods.entity.Member;
 
-@Getter @Setter
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Builder
 public class PaymentRequestDto {
 
-    private int quantity;
-    private int totalPrice;
-    private String accountBank;
-    private String accountNumber;
-    private Long goodsId;
-    private Long memberId;
-
-    private Goods goods;
-    private Member member;
+    @NotNull
+    private String paymentUid;
+    @NotNull
+    private String orderUid;
+    @NotNull
+    private int amount;
+    @NotNull
+    private String cardBank;
+    @NotNull
+    private String cardNumber;
 }
