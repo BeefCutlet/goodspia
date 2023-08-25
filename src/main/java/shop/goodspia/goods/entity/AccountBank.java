7adfd4d42c55010c74abacc49d35e88349116d37
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:02:48 +0900

    은행 코드 정보 추가, 코드로 검증 및 변환 메서드 추가

--- src/main/java/shop/goodspia/goods/entity/AccountBank.java
+++ src/main/java/shop/goodspia/goods/entity/AccountBank.java
@@ -1,23 +1,39 @@
 package shop.goodspia.goods.entity;
 
 public enum AccountBank {
-    KB("KB"),
-    SHINHAN("SHINHAN"),
-    NH("NH");
+    KB("KB", "04"),
+    SHINHAN("SHINHAN", "88"),
+    NH("NH", "11");
 
     private final String bankName;
+    private final String code;
 
-    AccountBank(String bankName) {
+    AccountBank(String bankName, String code) {
         this.bankName = bankName;
+        this.code = code;
     }
 
-    public static AccountBank covertToBank(String bank) {
+    public static AccountBank convertStringToBank(String bank) {
         if (bank == null) {
             throw new IllegalArgumentException("은행 이름이 입력되지 않았습니다.");
         }
 
         for (AccountBank accountBank : AccountBank.values()) {
             if (accountBank.bankName.equals(bank)) {
+                return accountBank;
+            }
+        }
+
+        throw new IllegalArgumentException("일치하는 은행이 없습니다.");
+    }
+
+    public static AccountBank convertCodeToBank(String code) {
+        if (code == null) {
+            throw new IllegalArgumentException("은행 코드가 입력되지 않았습니다.");
+        }
+
+        for (AccountBank accountBank : AccountBank.values()) {
+            if (accountBank.code.equals(code)) {
                 return accountBank;
             }
         }
