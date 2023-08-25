61c3065f6702887b66f4d1e8c1653791b850095d
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:16:09 +0900

    스프링 시큐리티 설정 변경

--- src/main/java/shop/goodspia/goods/security/config/SecurityConfig.java
+++ src/main/java/shop/goodspia/goods/security/config/SecurityConfig.java
@@ -11,6 +11,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .formLogin().disable()
                 .csrf().disable();
         return http.build();
     }
