38b2141f0acb5eed80e2f27407fc56a5c0679c6c
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:12:53 +0900

    이름 변경

--- src/main/java/shop/goodspia/goods/dto/payment/PaymentPrepareResponseDto.java
+++ src/main/java/shop/goodspia/goods/dto/payment/PaymentPrepareResponseDto.java
@@ -1,11 +1,11 @@
-package shop.goodspia.goods.dto.iamport;
+package shop.goodspia.goods.dto.payment;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class IAmPortResponseDto {
+public class PaymentPrepareResponseDto {
 
     private String merchant_uid;
     private int amount;
