03b6a46a62724b021fdd1c37c0ca97055f8d2c74
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:08:31 +0900

    주문 Repository 추가

---
+++ src/main/java/shop/goodspia/goods/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package shop.goodspia.goods.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import shop.goodspia.goods.entity.Orders;
+
+public interface OrderRepository extends JpaRepository<Orders, Long> {
+}
