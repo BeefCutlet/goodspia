61160656a60f3560d0151c31a86362e1d6c01a97
BeefCutlet <whysabi492@naver.com>
2023-08-26 01:14:01 +0900

    결제 Repository 추가

--- src/main/java/shop/goodspia/goods/repository/PaymentRepository.java
+++ src/main/java/shop/goodspia/goods/repository/PaymentRepository.java
@@ -1,7 +1,7 @@
 package shop.goodspia.goods.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import shop.goodspia.goods.entity.Payment;
+import shop.goodspia.goods.entity.Payments;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
+public interface PaymentRepository extends JpaRepository<Payments, Long> {
 }
