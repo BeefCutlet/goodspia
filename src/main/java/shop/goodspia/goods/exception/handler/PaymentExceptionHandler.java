0096602b3235fc5412c2b1bf9cf60683b7000b37
BeefCutlet <whysabi492@naver.com>
2023-08-28 12:36:49 +0900

    PaymentController import 경로 변경

--- src/main/java/shop/goodspia/goods/exception/handler/PaymentExceptionHandler.java
+++ src/main/java/shop/goodspia/goods/exception/handler/PaymentExceptionHandler.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import shop.goodspia.goods.api.PaymentController;
+import shop.goodspia.goods.controller.PaymentController;
 import shop.goodspia.goods.dto.Response;
 import shop.goodspia.goods.exception.PaymentValidationFailureException;
 
