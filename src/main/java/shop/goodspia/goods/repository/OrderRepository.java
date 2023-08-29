94d04e31ec0e3a566215d4dd2f3f483f69b81a27
BeefCutlet <whysabi492@naver.com>
2023-08-29 18:14:21 +0900

    커스텀 쿼리 메서드 추가

--- src/main/java/shop/goodspia/goods/repository/OrderRepository.java
+++ src/main/java/shop/goodspia/goods/repository/OrderRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import shop.goodspia.goods.entity.Orders;
 
 public interface OrderRepository extends JpaRepository<Orders, Long> {
+
+    Orders findByOrderUid(String orderUid);
 }
