1ee5fb3e7328853a995a57f56276beba2cba931c
BeefCutlet <whysabi492@naver.com>
2023-08-29 18:11:27 +0900

    반환 타입 변경 및 로그 추가

--- src/main/java/shop/goodspia/goods/controller/query/GoodsQueryController.java
+++ src/main/java/shop/goodspia/goods/controller/query/GoodsQueryController.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import shop.goodspia.goods.dto.Response;
 import shop.goodspia.goods.dto.goods.GoodsDetailResponseDto;
 import shop.goodspia.goods.dto.goods.GoodsResponseDto;
 import shop.goodspia.goods.service.GoodsService;
@@ -21,24 +19,41 @@ public class GoodsQueryController {
 
     private final GoodsService goodsService;
 
+    /**
+     * 최신 굿즈 리스트 - 메인페이지
+     * @param category
+     * @param pageable
+     * @return
+     */
     @GetMapping("/list")
-    public ResponseEntity<Page<GoodsResponseDto>> getGoodsList(Pageable pageable) {
-        log.info("GoodsQueryController.getGoodsList Start, pageSize={}, pageNum={}", pageable.getPageSize(), pageable.getPageNumber());
-        Page<GoodsResponseDto> goodsList = goodsService.getGoodsList(pageable);
-        return ResponseEntity.ok(goodsList);
+    public Response<Page<GoodsResponseDto>> getGoodsList(@RequestParam(required = false) String category, Pageable pageable) {
+        log.info("전체 굿즈 리스트 조회, pageSize={}, pageNum={}", pageable.getPageSize(), pageable.getPageNumber());
+        Page<GoodsResponseDto> goodsList = goodsService.getGoodsList(pageable, category);
+        return Response.of(HttpStatus.OK.value(), "굿즈 리스트 조회 성공", goodsList);
     }
 
+    /**
+     * 굿즈 상세 정보 조회 - 굿즈 상세페이지
+     * @param goodsId
+     * @return
+     */
     @GetMapping("/detail/{goodsId}")
-    public ResponseEntity<GoodsDetailResponseDto> getOneGoods(@PathVariable long goodsId) {
-        log.info("GoodsQueryController.getOneGoods Start, goodsId={}", goodsId);
+    public Response<GoodsDetailResponseDto> getOneGoods(@PathVariable long goodsId) {
+        log.info("굿즈 상세 정보 조회, goodsId={}", goodsId);
         GoodsDetailResponseDto goods = goodsService.getGoods(goodsId);
-        return ResponseEntity.ok(goods);
+        return Response.of(HttpStatus.OK.value(), "굿즈 상세 정보 조회 성공", goods);
     }
 
-    @GetMapping("/goods-list/{artistId}")
-    public ResponseEntity<Page<GoodsResponseDto>> getArtistGoodsList(@PathVariable long artistId, Pageable pageable) {
-        log.info("GoodsQueryController.getArtistGoodsList Start, artistId={}", artistId);
+    /**
+     * 아티스트가 제작한 굿즈 리스트 - 아티스트 페이지
+     * @param artistId
+     * @param pageable
+     * @return
+     */
+    @GetMapping("/artist/{artistId}")
+    public Response<Page<GoodsResponseDto>> getArtistGoodsList(@PathVariable long artistId, Pageable pageable) {
+        log.info("아티스트가 제작한 굿즈 리스트 조회, artistId={}", artistId);
         Page<GoodsResponseDto> artistGoodsList = goodsService.getArtistGoodsList(pageable, artistId);
-        return ResponseEntity.ok(artistGoodsList);
+        return Response.of(HttpStatus.OK.value(), "제작된 굿즈 리스트 조회 성공", artistGoodsList);
     }
 }
