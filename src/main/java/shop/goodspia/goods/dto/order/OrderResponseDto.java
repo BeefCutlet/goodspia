e3507448943913517b5be31627cf350616fa5165
BeefCutlet <whysabi492@naver.com>
2023-08-29 18:14:37 +0900

    빌더 패턴에서 생성자 방식으로 변경

--- src/main/java/shop/goodspia/goods/dto/order/OrderResponseDto.java
+++ src/main/java/shop/goodspia/goods/dto/order/OrderResponseDto.java
@@ -1,10 +1,9 @@
 package shop.goodspia.goods.dto.order;
 
-import lombok.Builder;
 import lombok.Getter;
+import shop.goodspia.goods.entity.OrderGoods;
 
 @Getter
-@Builder
 public class OrderResponseDto {
 
     private Long ordersGoodsId;
@@ -15,4 +14,15 @@ public class OrderResponseDto {
     private int goodsPrice;
     private String goodsImage;
     private String goodsDesign;
+
+    public OrderResponseDto(OrderGoods orderGoods) {
+        this.ordersGoodsId = orderGoods.getId();
+        this.quantity = orderGoods.getQuantity();
+        this.totalPrice = orderGoods.getTotalPrice();
+        this.goodsId = orderGoods.getGoods().getId();
+        this.goodsName = orderGoods.getGoods().getName();
+        this.goodsPrice = orderGoods.getGoods().getPrice();
+        this.goodsImage = orderGoods.getGoods().getThumbnail();
+        this.goodsDesign = orderGoods.getGoodsDesign();
+    }
 }
