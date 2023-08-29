8bfb7be9b81cf978947ac384a828601ff1412537
BeefCutlet <whysabi492@naver.com>
2023-08-29 18:09:32 +0900

    빌더 패턴에서 생성자 방식으로 변경

--- src/main/java/shop/goodspia/goods/dto/goods/GoodsDetailResponseDto.java
+++ src/main/java/shop/goodspia/goods/dto/goods/GoodsDetailResponseDto.java
@@ -1,10 +1,13 @@
 package shop.goodspia.goods.dto.goods;
 
 import lombok.Getter;
-import lombok.Setter;
+import shop.goodspia.goods.dto.design.DesignResponseDto;
+import shop.goodspia.goods.entity.Goods;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Setter
 public class GoodsDetailResponseDto {
 
     private Long goodsId;
@@ -15,4 +18,19 @@ public class GoodsDetailResponseDto {
     private String category;
     private int price;
     private String artistName;
+    private List<DesignResponseDto> goodsDesigns;
+
+    public GoodsDetailResponseDto(Goods goods) {
+        this.goodsId = goods.getId();
+        this.name = goods.getName();
+        this.summary = goods.getSummary();
+        this.mainImage = goods.getThumbnail();
+        this.content = goods.getContent();
+        this.category = goods.getCategory();
+        this.price = goods.getPrice();
+        this.artistName = goods.getArtist().getNickname();
+        this.goodsDesigns = goods.getDesigns().stream().map(
+                design -> new DesignResponseDto(design.getDesignName())
+        ).collect(Collectors.toList());
+    }
 }
