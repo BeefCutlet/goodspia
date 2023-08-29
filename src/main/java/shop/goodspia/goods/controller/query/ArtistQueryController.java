64736481e315da28d21cd0d18cb2f267768ba42e
BeefCutlet <whysabi492@naver.com>
2023-08-29 18:05:49 +0900

    URL 변경, 반환 타입 변경

--- src/main/java/shop/goodspia/goods/controller/query/ArtistQueryController.java
+++ src/main/java/shop/goodspia/goods/controller/query/ArtistQueryController.java
@@ -1,26 +1,25 @@
 package shop.goodspia.goods.controller.query;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.goodspia.goods.dto.Response;
 import shop.goodspia.goods.dto.artist.ArtistResponseDto;
 import shop.goodspia.goods.service.ArtistService;
-import shop.goodspia.goods.service.GoodsService;
 
 @RestController
-@RequestMapping("/artist")
+@RequestMapping("/artists")
 @RequiredArgsConstructor
 public class ArtistQueryController {
 
     private final ArtistService artistService;
-    private final GoodsService goodsService;
 
     @GetMapping("/{artistId}")
-    public ResponseEntity<ArtistResponseDto> getArtistInfo(@PathVariable long artistId) {
+    public Response<ArtistResponseDto> getArtistInfo(@PathVariable long artistId) {
         ArtistResponseDto artistInfo = artistService.getArtistInfo(artistId);
-        return ResponseEntity.ok(artistInfo);
+        return Response.of(HttpStatus.OK.value(), "아티스트 정보 조회 성공", artistInfo);
     }
 }
