e8a48b9ea665f65a7883550e51abc77447b4a2d3
BeefCutlet <whysabi492@naver.com>
2023-09-02 23:59:07 +0900

    Swagger 설정 추가

--- src/main/java/shop/goodspia/goods/config/SwaggerConfig.java
+++ src/main/java/shop/goodspia/goods/config/SwaggerConfig.java
@@ -1,33 +1,33 @@
 package shop.goodspia.goods.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo());
+    public GroupedOpenApi artistApi() {
+        return GroupedOpenApi.builder()
+                .group("Artist")
+                .packagesToScan("shop.goodspia.goods.controller")
+                .build();
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .version("v1.0.0")
                 .title("GoodsPia API")
-                .description("GoodsPia CRUD API")
-                .version("1.0")
-                .build();
+                .description("GoodsPia CRUD API");
     }
 }
