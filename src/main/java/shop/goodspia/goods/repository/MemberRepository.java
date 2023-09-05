82f84389826d32123fc2c426447f4f9f95c6a2f8
BeefCutlet <whysabi492@naver.com>
2023-09-05 18:07:45 +0900

    EntityGraph 추가

--- src/main/java/shop/goodspia/goods/repository/MemberRepository.java
+++ src/main/java/shop/goodspia/goods/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package shop.goodspia.goods.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import shop.goodspia.goods.entity.Member;
 
@@ -7,5 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    @EntityGraph(attributePaths = "artist")
     Optional<Member> findByEmail(String email);
 }
