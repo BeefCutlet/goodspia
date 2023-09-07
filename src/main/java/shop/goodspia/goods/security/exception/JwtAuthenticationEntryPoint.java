ced2b8f94bef49f9884bd9b68a161198a5a47cc3
BeefCutlet <whysabi492@naver.com>
2023-09-07 23:46:39 +0900

    AuthenticationEntryPoint exception 내용 표시 추가

--- src/main/java/shop/goodspia/goods/security/exception/JwtAuthenticationEntryPoint.java
+++ src/main/java/shop/goodspia/goods/security/exception/JwtAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package shop.goodspia.goods.security.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -8,11 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
+        log.info("Authorization Error, exception={}", authException.getMessage());
+        authException.printStackTrace();
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
