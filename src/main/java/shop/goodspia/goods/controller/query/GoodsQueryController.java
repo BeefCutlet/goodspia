f06fd307f7c206df66a78d41c14f812f5bd95556
BeefCutlet <whysabi492@naver.com>
2023-09-10 23:26:43 +0900

    반환값 변경

--- src/main/java/shop/goodspia/goods/controller/query/GoodsQueryController.java
+++ src/main/java/shop/goodspia/goods/controller/query/GoodsQueryController.java
@@ -1,16 +1,19 @@
 package shop.goodspia.goods.controller.query;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import shop.goodspia.goods.dto.Response;
 import shop.goodspia.goods.dto.goods.GoodsDetailResponse;
 import shop.goodspia.goods.dto.goods.GoodsResponse;
 import shop.goodspia.goods.service.GoodsService;
 
+@Tag(name = "굿즈 조회 API", description = "굿즈 목록/단건 조회 API")
 @Slf4j
 @RestController
 @RequestMapping("/goods")
@@ -25,11 +28,14 @@ public class GoodsQueryController {
      * @param pageable
      * @return
      */
+    @Operation(summary = "굿즈 목록 조회 API", description = "굿즈 목록을 조회하는 API. pageSize와 pageNum을 쿼리스트링으로 설정해야 합니다.")
     @GetMapping("/list")
-    public Response<Page<GoodsResponse>> getGoodsList(@RequestParam(required = false) String category, Pageable pageable) {
+    public ResponseEntity<Page<GoodsResponse>> getGoodsList(@Parameter(name = "굿즈 카테고리", description = "굿즈 검색 시 카테고리를 선택했다면 설정")
+                                                            @RequestParam(required = false) String category,
+                                                            @Parameter(hidden = true) Pageable pageable) {
         log.info("전체 굿즈 리스트 조회, pageSize={}, pageNum={}", pageable.getPageSize(), pageable.getPageNumber());
         Page<GoodsResponse> goodsList = goodsService.getGoodsList(pageable, category);
-        return Response.of(HttpStatus.OK.value(), "굿즈 리스트 조회 성공", goodsList);
+        return ResponseEntity.ok(goodsList);
     }
 
     /**
@@ -37,11 +43,12 @@ public class GoodsQueryController {
      * @param goodsId
      * @return
      */
+    @Operation(summary = "굿즈 단건 조회 API", description = "굿즈 상세 정보를 조회하기 위한 단건 조회 API")
     @GetMapping("/detail/{goodsId}")
-    public Response<GoodsDetailResponse> getOneGoods(@PathVariable long goodsId) {
+    public ResponseEntity<GoodsDetailResponse> getOneGoods(@Parameter(name = "굿즈 번호") @PathVariable Long goodsId) {
         log.info("굿즈 상세 정보 조회, goodsId={}", goodsId);
         GoodsDetailResponse goods = goodsService.getGoods(goodsId);
-        return Response.of(HttpStatus.OK.value(), "굿즈 상세 정보 조회 성공", goods);
+        return ResponseEntity.ok(goods);
     }
 
     /**
@@ -50,10 +57,13 @@ public class GoodsQueryController {
      * @param pageable
      * @return
      */
+    @Operation(summary = "굿즈 목록 조회 API",
+            description = "아티스트가 제작한 굿즈 목록을 조회하는 API. 조회할 아티스트의 번호를 파라미터로 주어야 합니다. pageSize와 pageNum을 쿼리스트링으로 설정해야 합니다.")
     @GetMapping("/artist/{artistId}")
-    public Response<Page<GoodsResponse>> getArtistGoodsList(@PathVariable long artistId, Pageable pageable) {
+    public ResponseEntity<Page<GoodsResponse>> getArtistGoodsList(@Parameter(name = "아티스트 번호") @PathVariable Long artistId,
+                                                                  @Parameter(hidden = true) Pageable pageable) {
         log.info("아티스트가 제작한 굿즈 리스트 조회, artistId={}", artistId);
         Page<GoodsResponse> artistGoodsList = goodsService.getArtistGoodsList(pageable, artistId);
-        return Response.of(HttpStatus.OK.value(), "제작된 굿즈 리스트 조회 성공", artistGoodsList);
+        return ResponseEntity.ok(artistGoodsList);
     }
 }
