89cc41e30c6426a14c7bbf9f20b25b1550106adb
BeefCutlet <whysabi492@naver.com>
2023-09-10 23:29:06 +0900

    응답 타입 변경, API 설명 수정

--- src/main/java/shop/goodspia/goods/controller/query/ArtistQueryController.java
+++ src/main/java/shop/goodspia/goods/controller/query/ArtistQueryController.java
@@ -1,16 +1,18 @@
 package shop.goodspia.goods.controller.query;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import shop.goodspia.goods.dto.Response;
 import shop.goodspia.goods.dto.artist.ArtistResponse;
 import shop.goodspia.goods.service.ArtistService;
 
+@Tag(name = "아티스트 정보 조회 API", description = "아티스트 정보 조회 API")
 @RestController
 @RequestMapping("/artists")
 @RequiredArgsConstructor
@@ -18,8 +20,9 @@ public class ArtistQueryController {
 
     private final ArtistService artistService;
 
+    @Operation(summary = "아티스트 단건 조회 API", description = "아티스트 번호로 아티스트 단건 정보를 조회합니다.")
     @GetMapping("/{artistId}")
-    public ResponseEntity<ArtistResponse> getArtistInfo(@PathVariable Long artistId) {
+    public ResponseEntity<ArtistResponse> getArtistInfo(@Parameter(name = "아티스트 번호") @PathVariable Long artistId) {
         ArtistResponse artistInfo = artistService.getArtistInfo(artistId);
         return ResponseEntity.ok(artistInfo);
     }
