060ece3551a24f62eec3b8048c69204381664290
BeefCutlet <whysabi492@naver.com>
2023-09-11 15:23:56 +0900

    응답 상태코드 변경

--- src/main/java/shop/goodspia/goods/controller/PaymentController.java
+++ src/main/java/shop/goodspia/goods/controller/PaymentController.java
@@ -16,6 +16,7 @@ import shop.goodspia.goods.service.PaymentAgentService;
 import shop.goodspia.goods.service.PaymentService;
 
 import javax.validation.Valid;
+import java.net.URI;
 
 @Tag(name = "결제 정보 사전등록/검증 및 저장 API", description = "결제 정보를 사전 등록하고, 사전등록된 정보를 바탕으로 검증 후 저장하는 API")
 @Slf4j
@@ -36,9 +37,9 @@ public class PaymentController {
      * @return
      * @throws Exception
      */
-    @Operation(summary = "결제 사전검증 API", description = "결제 진행 전 결제되어야 할 금액을 미리 저장하는 API")
+    @Operation(summary = "결제 사전검증 API", description = "결제 진행 전 결제되어야 할 금액을 미리 저장합니다.")
     @PostMapping("/reservation")
-    public ResponseEntity<PaymentPrepareResponse> reservePayment(@Parameter(name = "결제 정보", description = "추후 검증을 위해 사전에 등록할 결제 정보")
+    public ResponseEntity<PaymentPrepareResponse> reservePayment(@Parameter(description = "추후 검증을 위해 사전에 등록할 결제 정보")
                                                                  @RequestBody @Valid PaymentPrepareRequest preparePaymentInfo)
             throws Exception {
         PaymentPrepareResponse paymentPrepareResponse = paymentAgentService.reservePayment(preparePaymentInfo);
@@ -52,13 +53,13 @@ public class PaymentController {
      * @return
      * @throws Exception
      */
-    @Operation(summary = "결제 검증 API", description = "결제되어야 할 금액과 실제 결제된 금액이 일치하는지 유효성 검사를 하는 API")
+    @Operation(summary = "결제 검증 API", description = "결제되어야 할 금액과 실제 결제된 금액이 일치하는지 유효성 검사를 합니다.")
     @PostMapping("/validation/{paymentUid}")
-    public ResponseEntity<UrlResponse> validatePayment(@Parameter(name = "결제 UID", description = "결제 API 에서 결제 처리 후 생성된 결제 UID")
+    public ResponseEntity<UrlResponse> validatePayment(@Parameter(description = "결제 API 에서 결제 처리 후 생성된 결제 UID")
                                                        @PathVariable String paymentUid) throws Exception {
         PaymentRequest paymentRequest = paymentAgentService.validatePayment(paymentUid);
         paymentService.addPayment(paymentRequest);
 
-        return ResponseEntity.ok(UrlResponse.of(baseUrl));
+        return ResponseEntity.created(URI.create(baseUrl)).build();
     }
 }
