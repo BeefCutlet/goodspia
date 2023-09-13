55b0e0e35c22e624f2cf32b79e22d9037f0ec57b
BeefCutlet <whysabi492@naver.com>
2023-09-14 00:22:48 +0900

    Delivery 요청값 검증 추가

--- src/main/java/shop/goodspia/goods/dto/delivery/DeliverySaveRequest.java
+++ src/main/java/shop/goodspia/goods/dto/delivery/DeliverySaveRequest.java
@@ -2,18 +2,31 @@ package shop.goodspia.goods.dto.delivery;
 
 import lombok.Builder;
 import lombok.Getter;
+import shop.goodspia.goods.entity.DeliveryStatus;
 
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Getter
 @Builder
 public class DeliverySaveRequest {
 
+    @NotBlank
     private String deliveryNumber;
+
+    @NotBlank
+    @Pattern(regexp = "\\d{3}-\\d{2}")
     private String zipcode;
+
+    @NotBlank
+    @Pattern(regexp = "([가-힣]\\s[0-9])")
     private String addressDistrict;
+
+    @NotBlank
+    @Pattern(regexp = "[가-힣0-9a-zA-Z]+")
     private String addressDetail;
-    @Enumerated(EnumType.STRING)
-    private String deliveryStatus;
+
+    @NotNull
+    private DeliveryStatus deliveryStatus;
 }
