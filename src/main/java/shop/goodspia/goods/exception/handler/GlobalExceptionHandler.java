a147819599598fc9dd85b2755f115c31465174e4
BeefCutlet <whysabi492@naver.com>
2023-09-14 00:19:39 +0900

    기본 전역 예외 핸들링 메서드 추가

--- src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
@@ -1,20 +1,46 @@
 package shop.goodspia.goods.exception.handler;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import shop.goodspia.goods.dto.ErrorResponse;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import shop.goodspia.goods.dto.Response;
 
-@RestControllerAdvice
+@RestControllerAdvice(basePackages = "shop.goodspia.goods.controller")
 public class GlobalExceptionHandler {
 
+    //400 - 필드 검증 실패 에러
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<?> validationExHandling(MethodArgumentNotValidException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Response validationException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
-        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
-        return ResponseEntity.badRequest().body(errorResponse);
+        return Response.of(e.getMessage(), null);
+    }
+
+    //404
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public Response notFoundException(NoHandlerFoundException e) {
+        return Response.of(e.getMessage(), null);
+    }
+
+    //406
+    @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+    public Response httpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e) {
+        return Response.of(e.getMessage(), null);
+    }
+
+    //415
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    public Response httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        return Response.of(e.getMessage(), null);
     }
 
 }
