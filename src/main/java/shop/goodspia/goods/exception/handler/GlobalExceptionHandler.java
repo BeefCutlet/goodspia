9f7cdab41c8ce8780a0d25027d55ecfd530b6ca7
BeefCutlet <whysabi492@naver.com>
2023-09-16 12:11:58 +0900

    패키지 구성 변경에 따른 import 경로 변경

--- src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import shop.goodspia.goods.dto.Response;
+import shop.goodspia.goods.common.dto.Response;
 
 @RestControllerAdvice(basePackages = "shop.goodspia.goods.controller")
 public class GlobalExceptionHandler {
--- src/main/java/shop/goodspia/goods/exception/handler/OrderExceptionHandler.java
+++ src/main/java/shop/goodspia/goods/exception/handler/OrderExceptionHandler.java
@@ -2,13 +2,12 @@ package shop.goodspia.goods.exception.handler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import shop.goodspia.goods.controller.OrderController;
-import shop.goodspia.goods.controller.query.OrderQueryController;
-import shop.goodspia.goods.dto.Response;
+import shop.goodspia.goods.order.controller.OrderController;
+import shop.goodspia.goods.order.controller.OrderQueryController;
+import shop.goodspia.goods.common.dto.Response;
 
 @Slf4j
 @RestControllerAdvice(basePackageClasses = {OrderQueryController.class, OrderController.class})
