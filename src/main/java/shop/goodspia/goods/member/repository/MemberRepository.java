d787fa082777230283429a4679e5fc706739854f
BeefCutlet <whysabi492@naver.com>
2023-09-16 21:56:52 +0900

    이메일로 회원 정보 조회 쿼리 추가 (페치 조인 X)

--- src/main/java/shop/goodspia/goods/member/repository/MemberRepository.java
+++ src/main/java/shop/goodspia/goods/member/repository/MemberRepository.java
@@ -2,6 +2,7 @@ package shop.goodspia.goods.member.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import shop.goodspia.goods.member.entity.Member;
 
 import java.util.Optional;
@@ -10,4 +11,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @EntityGraph(attributePaths = "artist")
     Optional<Member> findByEmail(String email);
+
+    @Query("select m from Member m where m.email = :email")
+    Optional<Member> findByEmailNotFetch(String email);
 }
