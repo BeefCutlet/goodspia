40316339fa18728e1c3e5af83281a3c6d4f7d0a7
BeefCutlet <whysabi492@naver.com>
2023-09-27 19:01:52 +0900

    배포 자동화를 위한 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,12 +6,17 @@ cd $REPOSITORY
 APP_NAME=goodspia
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
+DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
 
 
 # =====================================
 # 현재 구동 중인 application pid 확인
 # =====================================
 CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
+echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 
 if [ -z "$CURRENT_PID" ]; then
     echo "NOT RUNNING"
@@ -22,4 +27,7 @@ else
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+
+sleep 2
+echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
