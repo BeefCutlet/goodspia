2d724173dd2075a27721d2dc2876967fd8ad7512
BeefCutlet <whysabi492@naver.com>
2023-09-27 20:03:23 +0900

    배포 자동화를 위한 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,10 +6,9 @@ cd $REPOSITORY
 APP_NAME=goodspia
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+DEPLOY_LOG_PATH="/home/ubuntu/$APP_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ubuntu/$APP_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ubuntu/$APP_NAME/application.log"
 
 
 # =====================================
@@ -21,13 +20,13 @@ echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 if [ -z "$CURRENT_PID" ]; then
     echo "NOT RUNNING"
 else
-    echo "> kill -9 $CURRENT_PID"
+    echo "> kill -15 $CURRENT_PID"
     kill -15 $CURRENT_PID
     sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH >> $APPLICATION_LOG_PATH 2> $DEPLOY_ERR_LOG_PATH &
+nohup java -jar $JAR_PATH 1>>$APPLICATION_LOG_PATH 2>$DEPLOY_ERR_LOG_PATH &
 
 sleep 2
 echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
