a954852e9efb1867b5ee9f1ad6ddc794b1c7ef19
BeefCutlet <whysabi492@naver.com>
2023-10-01 01:18:31 +0900

    배포 자동화를 위한 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,19 +14,19 @@ APPLICATION_LOG_PATH="/home/ubuntu/$APP_NAME/application.log"
 # =====================================
 # 현재 구동 중인 application pid 확인
 # =====================================
-CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
+CURRENT_PID=$(pgrep -f *.jar)
 echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -z $CURRENT_PID ]; then
     echo "NOT RUNNING"
 else
     echo "> kill -15 $CURRENT_PID"
-    kill -15 "$CURRENT_PID"
+    kill -15 $CURRENT_PID
     sleep 5
 fi
 
 echo "> $JAR_PATH 배포"
-nohup java -jar -Dspring.profiles.active=prod "$JAR_PATH" 1>>$APPLICATION_LOG_PATH 2>$DEPLOY_ERR_LOG_PATH &
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH 1>>$APPLICATION_LOG_PATH 2>$DEPLOY_ERR_LOG_PATH &
 
 sleep 2
 echo "> 배포 종료 : $(date +%c)" >> $DEPLOY_LOG_PATH
