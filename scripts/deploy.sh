2222e210e49ec50fdfa74844da49e982aa27302f
BeefCutlet <whysabi492@naver.com>
2023-10-01 00:18:22 +0900

    배포 자동화를 위한 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/goodspia-source
+cd $REPOSITORY
 
 APP_NAME=goodspia
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
@@ -13,14 +14,14 @@ APPLICATION_LOG_PATH="/home/ubuntu/$APP_NAME/application.log"
 # =====================================
 # 현재 구동 중인 application pid 확인
 # =====================================
-CURRENT_PID=$(pgrep -f .jar)
+CURRENT_PID=$(pgrep -fl demo | grep java | awk '{print $1}')
 echo "> 현재 동작중인 어플리케이션 pid 체크" >> $DEPLOY_LOG_PATH
 
 if [ -z "$CURRENT_PID" ]; then
     echo "NOT RUNNING"
 else
-    echo "> kill -9 $CURRENT_PID"
-    kill -9 "$CURRENT_PID"
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 "$CURRENT_PID"
     sleep 5
 fi
 
