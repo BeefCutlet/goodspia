600844480ae092e44336be173fac7b24ef7c970a
BeefCutlet <whysabi492@naver.com>
2023-10-04 02:10:29 +0900

    회원 정보 조회 API 추가

--- src/main/java/shop/goodspia/goods/member/controller/MemberQueryController.java
+++ src/main/java/shop/goodspia/goods/member/controller/MemberQueryController.java
@@ -1,12 +1,28 @@
 package shop.goodspia.goods.member.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import shop.goodspia.goods.member.dto.MemberResponse;
+import shop.goodspia.goods.member.entity.Member;
+import shop.goodspia.goods.member.service.MemberService;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
-@RequestMapping("/member")
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberQueryController {
 
+    private final MemberService memberService;
+
+    @GetMapping("/info")
+    public ResponseEntity<MemberResponse> getMemberInfo(HttpServletRequest request) {
+        Long memberId = (Long) request.getAttribute("memberId");
+        MemberResponse memberInfo = memberService.getMemberInfo(memberId);
+
+        return ResponseEntity.ok(memberInfo);
+    }
 }
