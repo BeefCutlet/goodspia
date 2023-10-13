8cafecfe78fd02ddd5ba3d41850c3b5c9e9603c8
BeefCutlet <whysabi492@naver.com>
2023-10-13 16:45:45 +0900

    예외 발생 시 반환 형식 변경

--- src/main/java/shop/goodspia/goods/security/handler/JwtAccessDeniedHandler.java
+++ src/main/java/shop/goodspia/goods/security/handler/JwtAccessDeniedHandler.java
@@ -1,18 +1,32 @@
 package shop.goodspia.goods.security.handler;
 
+import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import shop.goodspia.goods.common.dto.Response;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
+@Slf4j
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException)
             throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_FORBIDDEN);
+        Gson gson = new Gson();
+
+        log.info("Authentication Error, exception={}", accessDeniedException.getMessage());
+        //401 에러 반환
+        String errorResponse = gson.toJson(Response.of("접근 권한이 없습니다.", null));
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter().write(errorResponse);
     }
 }
--- src/main/java/shop/goodspia/goods/security/handler/JwtAuthenticationEntryPoint.java
+++ src/main/java/shop/goodspia/goods/security/handler/JwtAuthenticationEntryPoint.java
@@ -2,6 +2,7 @@ package shop.goodspia.goods.security.handler;
 
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -11,20 +12,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-    private Gson gson = new Gson();
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException)
             throws IOException, ServletException {
+        Gson gson = new Gson();
+
         log.info("Authorization Error, exception={}", authException.getMessage());
-        if (authException instanceof InsufficientAuthenticationException) {
-            String errorResponse = gson.toJson(Response.of("인증에 실패하였습니다.", null));
-            response.getWriter().write(errorResponse);
-        }
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        //401 에러 반환
+        String errorResponse = gson.toJson(Response.of("인증에 실패하였습니다.", null));
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.getWriter().write(errorResponse);
     }
 }
