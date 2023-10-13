bc0fa208bdac01d7de935445d45d12be72cf5d64
BeefCutlet <whysabi492@naver.com>
2023-10-13 16:49:07 +0900

    Auth 생성용 메서드 및 UPDATE용 메서드 추가

--- src/main/java/shop/goodspia/goods/security/entity/Auth.java
+++ src/main/java/shop/goodspia/goods/security/entity/Auth.java
@@ -1,20 +1,22 @@
 package shop.goodspia.goods.security.entity;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import shop.goodspia.goods.member.entity.Member;
 
 import javax.persistence.*;
 
-import static javax.persistence.FetchType.*;
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Auth {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "auth_id")
     private Long id;
     private String refreshToken;
@@ -22,4 +24,14 @@ public class Auth {
     @OneToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    public Auth(String refreshToken, Member member) {
+        this.refreshToken = refreshToken;
+        this.member = member;
+    }
+
+    public Auth updateRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+        return this;
+    }
 }
