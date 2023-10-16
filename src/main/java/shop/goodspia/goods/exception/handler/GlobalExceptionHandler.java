8eec864265d072e8aef9a70e4383d1da8c563d04
BeefCutlet <whysabi492@naver.com>
2023-10-16 16:05:31 +0900

    Response 타입 추가

--- src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/shop/goodspia/goods/exception/handler/GlobalExceptionHandler.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import shop.goodspia.goods.common.dto.Response;
 
-@RestControllerAdvice(basePackages = "shop.goodspia.goods.controller")
+@RestControllerAdvice(basePackages = "shop.goodspia.goods")
 public class GlobalExceptionHandler {
 
     //400 - 필드 검증 실패 에러
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Response validationException(MethodArgumentNotValidException e) {
+    public Response<?> validationException(MethodArgumentNotValidException e) {
         BindingResult bindingResult = e.getBindingResult();
         return Response.of(e.getMessage(), null);
     }
@@ -25,21 +25,21 @@ public class GlobalExceptionHandler {
     //404
     @ExceptionHandler(NoHandlerFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Response notFoundException(NoHandlerFoundException e) {
+    public Response<?> notFoundException(NoHandlerFoundException e) {
         return Response.of(e.getMessage(), null);
     }
 
     //406
     @ExceptionHandler(HttpMediaTypeNotAcceptableException.class)
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
-    public Response httpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e) {
+    public Response<?> httpMediaTypeNotAcceptableException(HttpMediaTypeNotAcceptableException e) {
         return Response.of(e.getMessage(), null);
     }
 
     //415
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-    public Response httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+    public Response<?> httpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
         return Response.of(e.getMessage(), null);
     }
 
