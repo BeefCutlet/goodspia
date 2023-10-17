e387e3b623a55e776d76da09f172cabad63b7d3f
BeefCutlet <whysabi492@naver.com>
2023-10-17 23:50:28 +0900

    Configuration 타입 변경

--- src/main/java/shop/goodspia/goods/common/config/RedisConfig.java
+++ src/main/java/shop/goodspia/goods/common/config/RedisConfig.java
@@ -3,6 +3,8 @@ package shop.goodspia.goods.common.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -23,8 +25,11 @@ public class RedisConfig {
     }
 
     @Bean
-    public LettuceConnectionFactory lettuceConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+    public RedisConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+        configuration.setHostName(host);
+        configuration.setPort(port);
+        return new LettuceConnectionFactory(configuration);
     }
 
     @Bean
@@ -36,7 +41,7 @@ public class RedisConfig {
 //                        .build());
 
         RedisTemplate<String, ?> template = new RedisTemplate<>();
-        template.setConnectionFactory(lettuceConnectionFactory());
+        template.setConnectionFactory(connectionFactory());
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
