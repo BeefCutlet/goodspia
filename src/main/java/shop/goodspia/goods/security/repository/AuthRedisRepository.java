86fdb48bc20112e79cc9c81d31ed0829c6660150
BeefCutlet <whysabi492@naver.com>
2023-10-23 16:08:03 +0900

    redis 명령 하나로 단축

--- src/main/java/shop/goodspia/goods/security/repository/AuthRedisRepository.java
+++ src/main/java/shop/goodspia/goods/security/repository/AuthRedisRepository.java
@@ -26,8 +26,7 @@ public class AuthRedisRepository {
         try {
             String key = keyGenerator.generateAuthKey(memberId);
             redisTemplate.delete(key);
-            redisTemplate.opsForValue().set(key, refreshToken);
-            redisTemplate.expire(key, 30, TimeUnit.MINUTES);
+            redisTemplate.opsForValue().set(key, refreshToken, 30, TimeUnit.MINUTES);
             log.info("Refresh 토큰 저장 성공, 회원 ID = {}", memberId);
         } catch (Exception e) {
             log.info("Refresh 토큰 저장 실패, 회원 ID = {}", memberId);
