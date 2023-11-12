11aa71f8d2589656aa0781bd3cb45e8fa7837fef
OSU <whysabi492@naver.com>
2023-11-12 16:42:07 +0900

    API 요청 시 https 적용

--- src/main/java/shop/goodspia/goods/GoodsApplication.java
+++ src/main/java/shop/goodspia/goods/GoodsApplication.java
@@ -1,10 +1,13 @@
 package shop.goodspia.goods;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://api.goodspia.shop", description = "Default Server URL")})
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
