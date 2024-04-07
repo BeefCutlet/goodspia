f8ea3ab0d3f8405ae9d06ce55daa7472af0ac8c3
OSU <whysabi492@naver.com>
2024-04-07 16:15:26 +0900

    chore: 샘플 데이터 변경

--- src/main/java/shop/goodspia/goods/SampleData.java
+++ src/main/java/shop/goodspia/goods/SampleData.java
@@ -1,30 +1,32 @@
 package shop.goodspia.goods;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-import shop.goodspia.goods.artist.entity.Artist;
-import shop.goodspia.goods.cart.entity.Cart;
-import shop.goodspia.goods.common.dto.AccountBank;
-import shop.goodspia.goods.goods.entity.Design;
-import shop.goodspia.goods.goods.entity.Goods;
-import shop.goodspia.goods.member.entity.Member;
-import shop.goodspia.goods.order.entity.OrderGoods;
-import shop.goodspia.goods.order.entity.Orders;
-import shop.goodspia.goods.payment.entity.Payments;
-
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-import java.util.ArrayList;
-import java.util.List;
-
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//import shop.goodspia.goods.artist.entity.Artist;
+//import shop.goodspia.goods.cart.entity.Cart;
+//import shop.goodspia.goods.common.dto.AccountBank;
+//import shop.goodspia.goods.goods.entity.Design;
+//import shop.goodspia.goods.goods.entity.Goods;
+//import shop.goodspia.goods.member.entity.Address;
+//import shop.goodspia.goods.member.entity.Gender;
+//import shop.goodspia.goods.member.entity.Member;
+//import shop.goodspia.goods.member.entity.MemberStatus;
+//import shop.goodspia.goods.order.entity.OrderGoods;
+//import shop.goodspia.goods.order.entity.Orders;
+//
+//import javax.annotation.PostConstruct;
+//import javax.persistence.EntityManager;
+//import java.util.ArrayList;
+//import java.util.List;
+//
 //@Component
 //@RequiredArgsConstructor
-//@Profile("local")
+//@Profile("dev")
 public class SampleData {
-
+//
 //    private final Sample sample;
 //
 //    @PostConstruct
@@ -41,39 +43,51 @@ public class SampleData {
 //        private final PasswordEncoder passwordEncoder;
 //
 //        public void sampleInit() {
-//            for (int k = 1; k <= 20; k++) {
+//            for (int k = 201; k <= 300; k++) {
+//                Member member = Member.builder()
+//                        .email("member" + k +"@email.com")
+//                        .password(passwordEncoder.encode("memberpassword" + k))
+//                        .nickname("닉네임" + k)
+//                        .name("김땡땡")
+//                        .gender((k % 2 == 0) ? Gender.MAN : Gender.WOMAN)
+//                        .birthday("20000101")
+//                        .phoneNumber("010-1234-1234")
+//                        .address(Address.of(
+//                                "01234",
+//                                "서울특별시 하나시 둘로11",
+//                                "하나빌라 101호"))
+//                        .status(MemberStatus.ACTIVE)
+//                        .build();
+//                em.persist(member);
 //                Artist artist = Artist.builder()
-//                        .nickname("ArtistNickname" + k)
-//                        .profileImage("aaaaaaaa-ArtistProfileImage.png")
+//                        .nickname("아티스트" + k)
+//                        .profileImage("https://cdn.vuetifyjs.com/images/cards/sunshine.jpg")
 //                        .accountBank(AccountBank.KB)
 //                        .accountNumber("123123-00-123123")
 //                        .phoneNumber("010-0000-0000")
 //                        .build();
 //                em.persist(artist);
 //
-//                Member member = Member.builder()
-//                        .nickname("MemberNickname" + k)
-//                        .email("MemberEmail" + k + "@email.com")
-//                        .password(passwordEncoder.encode("MemberPassword"))
-//                        .artist(artist)
-//                        .build();
-//                em.persist(member);
 //
-//                for (int i = 1; i <= 100; i++) {
+//                for (int i = 1; i <= 20; i++) {
 //                    Goods goods = Goods.builder()
-//                            .name("GoodsName" + i)
-//                            .thumbnail("aaaaaaaa-GoodsImage.png" + i)
-//                            .category("GoodsCategory" + i)
-//                            .summary("GoodsSummary" + i)
-//                            .content("GoodsContent" + i)
+//                            .name("굿즈" + (int) (Math.random() * 10000))
+//                            .content("굿즈 내용입니다." + k)
 //                            .price(10000)
+//                            .stock(100)
+//                            .thumbnail((k % 3 == 0)
+//                                    ? "https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+//                                    : (k % 3 == 1)
+//                                    ? "https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+//                                    : "https://picsum.photos/500/300?image=232")
+//                            .isDeleted(0)
 //                            .artist(artist)
 //                            .build();
 //                    em.persist(goods);
 //
-//                    for (int j = 1; j <= 2; j++) {
+//                    for (int j = 1; j <= 3; j++) {
 //                        Design design = Design.builder()
-//                                .designName("DesignName" + j)
+//                                .designName("디자인" + j)
 //                                .goods(goods)
 //                                .build();
 //                        em.persist(design);
@@ -87,25 +101,23 @@ public class SampleData {
 //                        em.persist(cart);
 //                    }
 //
-//                    for (int l = 1; l <= 10; l++) {
-//                        List<OrderGoods> orderGoodsList = new ArrayList<>();
-//                        OrderGoods orderGoods1 = OrderGoods.createOrderGoods(
-//                                goods, i, i * 10000, "DesignName1");
-//                        em.persist(orderGoods1);
+//                    List<OrderGoods> orderGoodsList = new ArrayList<>();
+//                    OrderGoods orderGoods1 = OrderGoods.from(
+//                            goods, i, i * 10000, "디자인1");
+//                    em.persist(orderGoods1);
 //
-//                        OrderGoods orderGoods2 = OrderGoods.createOrderGoods(
-//                                goods, i, i * 10000, "DesignName2");
-//                        em.persist(orderGoods2);
+//                    OrderGoods orderGoods2 = OrderGoods.from(
+//                            goods, i, i * 10000, "디자인2");
+//                    em.persist(orderGoods2);
 //
-//                        orderGoodsList.add(orderGoods1);
-//                        orderGoodsList.add(orderGoods2);
+//                    orderGoodsList.add(orderGoods1);
+//                    orderGoodsList.add(orderGoods2);
 //
-//                        Orders order = Orders.createOrder(member, orderGoodsList);
-//                        em.persist(order);
-//                    }
+//                    Orders order = Orders.from(member, orderGoodsList);
+//                    em.persist(order);
 //                }
+//                em.flush();
 //            }
-//
 //        }
 //    }
 }
