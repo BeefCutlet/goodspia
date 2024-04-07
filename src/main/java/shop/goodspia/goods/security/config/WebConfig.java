d884059f8abf4b8769dfbc95250b48337da4d016
OSU <whysabi492@naver.com>
2024-04-08 01:44:27 +0900

    fix: CORS 에러 수정

---
+++ src/main/java/shop/goodspia/goods/security/config/WebConfig.java
@@ -0,0 +1,19 @@
+package shop.goodspia.goods.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://goodspia.shop", "http://localhost:3000")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3000);
+    }
+}
