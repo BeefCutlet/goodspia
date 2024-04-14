b8ac010d5a1387d17c94ab661ab7e5742e6a44a5
OSU <whysabi492@naver.com>
2024-04-14 15:27:54 +0900

    fix: 성별 ENUM 타입을 검증하지 못했던 에러 수정

--- src/main/java/shop/goodspia/goods/member/entity/Gender.java
+++ src/main/java/shop/goodspia/goods/member/entity/Gender.java
@@ -1,6 +1,31 @@
 package shop.goodspia.goods.member.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import shop.goodspia.goods.common.exception.InvalidRequestException;
+import shop.goodspia.goods.common.exception.dto.ErrorCode;
+
 public enum Gender {
-    MAN,
-    WOMAN
+    MAN("남성"),
+    WOMAN("여성")
+    ;
+
+    private final String description;
+
+    Gender(String description) {
+        this.description = description;
+    }
+
+    @JsonCreator
+    public static Gender from(String requestGender) {
+        for (Gender gender : Gender.values()) {
+            if (gender.name().equals(requestGender)) {
+                return gender;
+            }
+        }
+        throw new InvalidRequestException(ErrorCode.INVALID_GENDER);
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
