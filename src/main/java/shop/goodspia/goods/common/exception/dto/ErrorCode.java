db2c235369a20523fe2d1aa313471ec7452dba2b
OSU <whysabi492@naver.com>
2024-04-14 15:28:20 +0900

    chore: 에러 코드 추가

--- src/main/java/shop/goodspia/goods/common/exception/dto/ErrorCode.java
+++ src/main/java/shop/goodspia/goods/common/exception/dto/ErrorCode.java
@@ -4,13 +4,19 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
 
-    NOT_VALID_TOKEN(HttpStatus.FORBIDDEN, "유효하지 않은 토큰입니다."),
+    INVALID_TOKEN(HttpStatus.FORBIDDEN, "유효하지 않은 토큰입니다."),
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "토큰을 발견하지 못했습니다."),
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "회원 정보를 찾을 수 없습니다."),
     BAD_CREDENTIALS(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    INVALID_GENDER(HttpStatus.BAD_REQUEST, "성별 정보가 유효하지 않습니다."),
+
+    METHOD_ARGS_NOT_VALID(HttpStatus.BAD_REQUEST, "요청 정보가 유효하지 않습니다."),
+    HANDLER_NOT_FOUND(HttpStatus.BAD_REQUEST, "처리할 수 없는 요청입니다."),
+    INVALID_MEDIA_TYPE(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "잘못된 미디어 타입입니다."),
     ;
 
-    private HttpStatus status;
-    private String message;
+    private final HttpStatus status;
+    private final String message;
 
     ErrorCode(HttpStatus status, String message) {
         this.status = status;
