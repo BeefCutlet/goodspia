bb78678a5d443f07954b3c33ddbb6002e30967f3
OSU <whysabi492@naver.com>
2024-04-14 15:27:54 +0900

    fix: 성별 ENUM 타입을 검증하지 못했던 에러 수정

--- src/main/java/shop/goodspia/goods/common/validator/EnumValidator.java
+++ src/main/java/shop/goodspia/goods/common/validator/EnumValidator.java
@@ -1,24 +1,29 @@
 package shop.goodspia.goods.common.validator;
 
+import lombok.extern.slf4j.Slf4j;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Arrays;
 
+@Slf4j
 public class EnumValidator implements ConstraintValidator<ValidEnum, Enum> {
 
-    private ValidEnum annotation;
+    private Object[] enumValues;
 
     @Override
     public void initialize(ValidEnum constraintAnnotation) {
-        this.annotation = constraintAnnotation;
+        this.enumValues = constraintAnnotation.enumClass().getEnumConstants();
     }
 
     @Override
     public boolean isValid(Enum value, ConstraintValidatorContext context) {
         boolean result = false;
-        Object[] enumValues = this.annotation.enumClass().getEnumConstants();
+        log.info(Arrays.toString(enumValues));
         if (enumValues != null) {
             for (Object enumValue : enumValues) {
                 if (value == enumValue) {
+                    log.info("enum value: {}", enumValue);
                     result = true;
                     break;
                 }
