8f252da35e765eb1f4368b45b185e050675e31d3
OSU <whysabi492@naver.com>
2024-04-14 15:49:58 +0900

    chore: 인증 Controller 이름 변경

--- src/main/java/shop/goodspia/goods/member/controller/MemberController.java
+++ src/main/java/shop/goodspia/goods/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package shop.goodspia.goods.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import shop.goodspia.goods.member.service.MemberService;
 import shop.goodspia.goods.security.dto.AccessToken;
 import shop.goodspia.goods.security.dto.AuthResponse;
 import shop.goodspia.goods.security.dto.MemberPrincipal;
+import shop.goodspia.goods.security.dto.TokenInfo;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -37,9 +40,12 @@ public class MemberController {
         //회원 가입
         AuthResponse authResponse = memberService.saveMember(memberInfo);
 
+        //리프레시 토큰 생성
+        ResponseCookie refreshTokenCookie = createRefreshTokenCookie(authResponse.getRefreshToken());
+
         //회원 가입 성공 시 액세스 토큰, 리프레시 토큰(쿠키) 발급
         return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, authResponse.getRefreshToken())
+                .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
                 .body(authResponse.getAccessToken());
     }
 
@@ -52,5 +58,16 @@ public class MemberController {
         Long memberId = memberPrincipal.getId();
         memberService.modifyMemberInfo(memberId, memberInfo);
         return ResponseEntity.created(URI.create(baseUrl + "/members/info")).build();
+    }
+
+    //Refresh 토큰 쿠키 생성 메서드 - HttpOnly, Secure 설정
+    private ResponseCookie createRefreshTokenCookie(String refreshToken) {
+        return ResponseCookie.from(TokenInfo.REFRESH_TOKEN, refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .sameSite(SameSiteCookies.STRICT.getValue())
+                .domain("goodspia.shop")
+                .maxAge(TokenInfo.REFRESH_EXP)
+                .build();
     }
 }
