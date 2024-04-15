166932aa26844bb0e915c2e01d10afcfc73b4412
OSU <whysabi492@naver.com>
2024-04-15 18:41:32 +0900

    chore: 굿즈 리스트 조회 반환 스펙 변경

--- src/main/java/shop/goodspia/goods/goods/dto/GoodsResponse.java
+++ src/main/java/shop/goodspia/goods/goods/dto/GoodsResponse.java
@@ -3,8 +3,6 @@ package shop.goodspia.goods.goods.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.constraints.*;
-
 @Getter @Setter
 public class GoodsResponse {
 
@@ -12,4 +10,5 @@ public class GoodsResponse {
     private String thumbnail;
     private String goodsName;
     private int price;
+    private String artistNickname;
 }
