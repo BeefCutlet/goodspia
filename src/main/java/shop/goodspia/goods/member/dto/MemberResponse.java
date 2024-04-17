e485d2bfde7348f239eb3a78f1c944df34b2ce7c
OSU <whysabi492@naver.com>
2024-04-17 23:04:28 +0900

    chore: 회원 정보 응답 스펙 변경

--- src/main/java/shop/goodspia/goods/member/dto/MemberResponse.java
+++ src/main/java/shop/goodspia/goods/member/dto/MemberResponse.java
@@ -1,8 +1,9 @@
 package shop.goodspia.goods.member.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import shop.goodspia.goods.member.entity.Address;
+import shop.goodspia.goods.member.entity.Gender;
 import shop.goodspia.goods.member.entity.Member;
 
 @Getter
@@ -12,16 +13,20 @@ public class MemberResponse {
     private String email;
     private String nickname;
     private String name;
+    private Gender gender;
     private String phoneNumber;
     private String birthday;
+    private Address address;
 
     public static MemberResponse from(Member member) {
         return MemberResponse.builder()
                 .email(member.getEmail())
                 .nickname(member.getNickname())
                 .name(member.getName())
+                .gender(member.getGender())
                 .phoneNumber(member.getBirthday())
                 .birthday(member.getBirthday())
+                .address(member.getAddress())
                 .build();
     }
 }
