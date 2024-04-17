8dc6da48fcaf2d4daabe65e7d4e95d844bd3c8c6
OSU <whysabi492@naver.com>
2024-04-17 23:04:28 +0900

    chore: 회원 정보 응답 스펙 변경

--- src/main/java/shop/goodspia/goods/member/controller/MemberQueryController.java
+++ src/main/java/shop/goodspia/goods/member/controller/MemberQueryController.java
@@ -2,13 +2,13 @@ package shop.goodspia.goods.member.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shop.goodspia.goods.member.dto.MemberResponse;
 import shop.goodspia.goods.member.service.MemberService;
-
-import javax.servlet.http.HttpServletRequest;
+import shop.goodspia.goods.security.dto.MemberPrincipal;
 
 @RestController
 @RequestMapping("/members")
@@ -18,8 +18,8 @@ public class MemberQueryController {
     private final MemberService memberService;
 
     @GetMapping("/info")
-    public ResponseEntity<MemberResponse> getMemberInfo(HttpServletRequest request) {
-        Long memberId = (Long) request.getAttribute("memberId");
+    public ResponseEntity<MemberResponse> getMemberInfo(@AuthenticationPrincipal MemberPrincipal principal) {
+        Long memberId = principal.getId();
         MemberResponse memberInfo = memberService.getMemberInfo(memberId);
 
         return ResponseEntity.ok(memberInfo);
