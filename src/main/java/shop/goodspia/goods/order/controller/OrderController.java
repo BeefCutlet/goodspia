fea1d73087fe70e7e6d1f3decd70f65ff3c0c9bd
OSU <whysabi492@naver.com>
2024-04-18 20:18:26 +0900

    refactor: 코드 간소화

--- src/main/java/shop/goodspia/goods/order/controller/OrderController.java
+++ src/main/java/shop/goodspia/goods/order/controller/OrderController.java
@@ -4,11 +4,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import shop.goodspia.goods.order.dto.OrderSaveListRequest;
 import shop.goodspia.goods.order.service.OrderService;
+import shop.goodspia.goods.security.dto.MemberPrincipal;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Slf4j
@@ -24,25 +25,18 @@ public class OrderController {
 
     /**
      * DB에 주문 정보 저장
-     * 굿즈 상세페이지의 결제하기 버튼, 장바구니의 결제하기 버튼 클릭 시 동작
-     *
-     * @param orderList
-     * @param request
-     * @return 주문 목록 페이지로 이동 URL
+     * 굿즈 상세페이지의 주문하기 버튼, 장바구니의 주문하기 버튼 클릭 시 동작
      */
     @PostMapping
     public ResponseEntity<?> addOrders(@RequestBody @Valid OrderSaveListRequest orderList,
-                                       HttpServletRequest request) {
-        Long memberId = (Long) request.getAttribute("memberId");
-        Long orderId = orderService.addOrders(orderList, memberId);
+                                       @AuthenticationPrincipal MemberPrincipal principal) {
+        Long memberId = principal.getId();
+        orderService.addOrders(orderList, memberId);
         return ResponseEntity.noContent().build();
     }
 
     /**
      * 주문 굿즈 목록에서 주문 굿즈 삭제
-     *
-     * @param orderGoodsId
-     * @return
      */
     @DeleteMapping("/{orderGoodsId}")
     public ResponseEntity<String> deleteOrder(@PathVariable Long orderGoodsId) {
