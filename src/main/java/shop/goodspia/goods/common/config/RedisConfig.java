d435d1c3ed002455a058f20fcd896acdbd3bbc76
OSU <whysabi492@naver.com>
2024-04-19 11:37:55 +0900

    chore: 안 쓰는 코드 주석화 및 배포를 위한 설정 변경

--- src/main/java/shop/goodspia/goods/common/config/RedisConfig.java
+++ src/main/java/shop/goodspia/goods/common/config/RedisConfig.java
@@ -17,45 +17,45 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
-@Configuration
-@EnableRedisRepositories
+//@Configuration
+//@EnableRedisRepositories
 public class RedisConfig {
 
-    private String host;
-    private int port;
-
-    public RedisConfig(@Value("${spring.redis.host}") String host,
-                       @Value("${spring.redis.port}") int port) {
-        this.host = host;
-        this.port = port;
-    }
-
-    @Bean
-    public RedisConnectionFactory connectionFactory() {
-        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
-        configuration.setHostName(host);
-        configuration.setPort(port);
-        return new LettuceConnectionFactory(configuration);
-    }
-
-    @Bean
-    public RedisTemplate<String, ?> redisTemplate() {
-        RedisTemplate<String, ?> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory());
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return template;
-    }
-
-    @Bean
-    public CacheManager cacheManager() {
-        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
-                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
-                .entryTtl(Duration.ofMinutes(3L));
-
-        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory()).cacheDefaults(redisCacheConfiguration).build();
-    }
+//    private String host;
+//    private int port;
+//
+//    public RedisConfig(@Value("${spring.redis.host}") String host,
+//                       @Value("${spring.redis.port}") int port) {
+//        this.host = host;
+//        this.port = port;
+//    }
+//
+//    @Bean
+//    public RedisConnectionFactory connectionFactory() {
+//        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration();
+//        configuration.setHostName(host);
+//        configuration.setPort(port);
+//        return new LettuceConnectionFactory(configuration);
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, ?> redisTemplate() {
+//        RedisTemplate<String, ?> template = new RedisTemplate<>();
+//        template.setConnectionFactory(connectionFactory());
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        template.setHashKeySerializer(new StringRedisSerializer());
+//        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//        return template;
+//    }
+//
+//    @Bean
+//    public CacheManager cacheManager() {
+//        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+//                .entryTtl(Duration.ofMinutes(3L));
+//
+//        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(connectionFactory()).cacheDefaults(redisCacheConfiguration).build();
+//    }
 }
